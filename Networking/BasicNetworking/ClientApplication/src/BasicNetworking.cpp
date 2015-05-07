f1eddb3429e4d591a0d6a8b0f71f670a5beb6f78
Daniel Hardy <flamingarro@hotmail.com>
2015-05-07 16:54:50 +1000

    tut + storing connected clients system adressess

--- Networking/BasicNetworking/ClientApplication/src/BasicNetworking.cpp
+++ Networking/BasicNetworking/ClientApplication/src/BasicNetworking.cpp
@@ -6,6 +6,7 @@
 #include "RakPeerInterface.h"
 #include "MessageIdentifiers.h"
 #include "BitStream.h"
+#include "../../ServerApplication/GameMessages.h"
 
 
 BasicNetworkingApplication::BasicNetworkingApplication()
@@ -23,6 +24,9 @@ bool BasicNetworkingApplication::startup()
 	//Setup the basic window
 	createWindow("Client Application", 1280, 720);
 
+	//connect to server
+	handleNetworkConnection();
+
 	return true;
 }
 
@@ -33,6 +37,7 @@ void BasicNetworkingApplication::shutdown()
 
 bool BasicNetworkingApplication::update(float deltaTime)
 {
+	handleNetworkMessages();
 
 	return true;
 }
@@ -40,4 +45,78 @@ bool BasicNetworkingApplication::update(float deltaTime)
 void BasicNetworkingApplication::draw()
 {
 
+}
+
+void BasicNetworkingApplication::handleNetworkConnection()
+{
+	//Initialize the Raknet peer interface first
+	m_pPeerInterface = RakNet::RakPeerInterface::GetInstance();
+	initializeClientConnection();
+}
+
+void BasicNetworkingApplication::initializeClientConnection()
+{
+	//Create a socket discriptor to describe this connection
+	//No data needed, as we will be connecting to a server
+	RakNet::SocketDescriptor sd;
+
+	//Now call startup - max of 1 connections (to the server)
+	m_pPeerInterface->Startup(1, &sd, 1);
+
+	std::cout << "Connecting to the server at: " << IP << std::endl;
+
+	//Npw call connect to attempt to connect to the given server
+	RakNet::ConnectionAttemptResult res = m_pPeerInterface->Connect(IP, PORT, nullptr, 0);
+
+	//Finally, check to see if we connected, and if not, throw an error
+	if (res != RakNet::CONNECTION_ATTEMPT_STARTED)
+	{
+		std::cout << "Unable to start connection, Error number: " << res << std::endl;
+	}
+}
+
+void BasicNetworkingApplication::handleNetworkMessages()
+{
+	RakNet::Packet* packet;
+
+	for (packet = m_pPeerInterface->Receive(); packet; m_pPeerInterface->DeallocatePacket(packet), packet = m_pPeerInterface->Receive())
+	{
+		switch (packet->data[0])
+		{
+		case ID_REMOTE_DISCONNECTION_NOTIFICATION:
+			std::cout << "Another client has disconnected.\n";
+			break;
+		case ID_REMOTE_CONNECTION_LOST:
+			std::cout << "Another client has lost connection.\n";
+			break;
+		case ID_REMOTE_NEW_INCOMING_CONNECTION:
+			std::cout << "Another client has connected.\n";
+			break;
+		case ID_CONNECTION_REQUEST_ACCEPTED:
+			std::cout << "Our connect request has been accepted.\n";
+			break;
+		case ID_NO_FREE_INCOMING_CONNECTIONS:
+			std::cout << "The server is full.\n";
+			break;
+		case ID_DISCONNECTION_NOTIFICATION:
+			std::cout << "We have been disconnected.\n";
+			break;
+		case ID_CONNECTION_LOST:
+			std::cout << "Connection lost.\n";
+			break;
+		case ID_SERVER_TEXT_MESSAGE:
+		{
+			RakNet::BitStream bsIn(packet->data, packet->length, false);
+			bsIn.IgnoreBytes(sizeof(RakNet::MessageID));
+
+			RakNet::RakString str;
+			bsIn.Read(str);
+			std::cout << str.C_String() << std::endl;
+			break;
+		}
+		default:
+			std::cout << "Received a message with an unknown id: " << packet->data[0] << std::endl;
+			break;
+		}
+	}
 }
--- Networking/BasicNetworking/ClientApplication/src/BasicNetworking.h
+++ Networking/BasicNetworking/ClientApplication/src/BasicNetworking.h
@@ -21,5 +21,20 @@ public:
 
 	virtual void draw();
 
+	//network functions
+
+	//init the connection
+	void handleNetworkConnection();
+	void initializeClientConnection();
+
+	//handle incoming packets
+	void handleNetworkMessages();
+public:
+	RakNet::RakPeerInterface* m_pPeerInterface;
+
+	const char* IP = "127.0.0.1";
+	const unsigned short PORT = 5456;
+
 private:
+
 };
